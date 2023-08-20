479179ed73582e8a53055c029503dd36ebd4af35
Wexa <calledhypocrisy@gmail.com>
2023-08-20 22:46:23 +0300

    column gap fix, tooltip zindex fix

--- source/components/Column/index.tsx
+++ source/components/Column/index.tsx
@@ -5,12 +5,15 @@ import { Props } from "../../utility/props";
 import "./index.scss"
 
 export const Column = ( props ) => {
-	let { className, children, style, flex, stretch, ...rest } = props;
+	let { className, children, style, flex, stretch, gap, ...rest } = props;
 
 	let inlineStyle = { ...style };
 
 	if( flex )
 		inlineStyle[ "flex" ] = Props.parseFlex( flex );
+
+	if( gap )
+		inlineStyle[ "gap" ] = Props.parseGap( gap );
 
 	if( stretch )
 		inlineStyle[ "height" ] = "100%";
