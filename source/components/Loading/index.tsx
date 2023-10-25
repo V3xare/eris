a08259e83208592bea371a7a6990147043a438ab
Wexa <calledhypocrisy@gmail.com>
2023-10-25 14:59:53 +0300

    added loading component

--- source/components/Loading/index.tsx
+++ source/components/Loading/index.tsx
@@ -2,19 +2,22 @@ import React, { useContext } from "react";
 import { Props } from "../../utility/props";
 import Common from "../../utility/common";
 import { Icons } from "../../components/Icons";
+import { Text } from "../../components/Typography";
 import { Lang, LangContext } from "../../components/Lang";
 
 import "./index.scss"
 
 export const Loading = ( props ) => {
-	let { children, icon, tip, title, ...rest } = props;
+	let { children, icon, tip, title, status, ...rest } = props;
 	const lang = useContext( LangContext );
 
 	return (<div className={ Props.className( "loading" ) }>
-		<div className={ Props.className( "loading-overlay" ) }>
+		<Text className={ Props.className( "loading-overlay", { hidden: status !== true } ) }>
 			{ icon ? icon : (<Icons.spinner spin size={ 24 }/>) }
 			{ (tip || title) ? (tip || title) : (lang.get( "loading" ) || "Loading...") }
+		</Text>
+		<div className={ Props.className( "loading-container", { active: status === true } ) }>
+			{ children }
 		</div>
-		{ children }
 	</div>);
 };
--- source/examples/routes/loading.route.tsx
+++ source/examples/routes/loading.route.tsx
@@ -1,18 +1,40 @@
-import React, { useMemo, useReducer, useContext, useEffect } from "react";
+import React, { useMemo, useReducer, useContext, useEffect, useState } from "react";
 
 import { Card } from "../../components/Card";
 import { Divider } from "../../components/Divider";
 import { Loading } from "../../components/Loading";
+import { Input } from "../../components/Input";
+import { Column } from "../../components/Column";
+import { Editable } from "../../components/Editable";
+import { Text } from "../../components/Typography";
 
 export const LoadingRoute = ( props ) => {
+
+	const [ loading, setLoading ] = useState( true );
 	
 	return (
 		<Card borderless padding={ 0 }>
 
 			<Card>
 				<Divider>Lang</Divider>	
-				<Loading>
-					<Card></Card>
+				<div onClick={() => setLoading( !loading ) }>{ loading ? "loading" : "not loading" }</div>
+				<Loading status={ loading }>
+					<Card>
+						<Column>
+							<Input onChange={( e ) => console.log( e.value ) }>{ "Hello World" }</Input>
+							<Divider>Raw flex style</Divider>
+
+							<Editable>Hello World</Editable>
+							<Text>Hello World</Text>
+							<Text strong>Hello World</Text>
+
+							<Divider>Raw flex style</Divider>
+
+							<Text italic>Hello World</Text>
+							<Text italic strong>Hello World</Text>
+							<Text link={ "/link" } italic>Hello World</Text>
+						</Column>
+					</Card>
 				</Loading>
 			</Card>
 
