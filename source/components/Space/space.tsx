c0eeccb77b801c6b28d2269dfd4ed5449030c7db
Vexare <calledhypocrisy@gmail.com>
2024-08-21 04:26:12 +0300

    1.0.113

--- source/components/Space/space.tsx
+++ source/components/Space/space.tsx
@@ -1,6 +1,8 @@
 
 import React, { useReducer, useState, useEffect, useMemo, useContext } from "react";
 
+import "./space.scss"
+
 export const Space = ( props ) => {
 	return <span className="space"></span>
 };
